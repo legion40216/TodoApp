a35bbebda0e23a2ad9ab6f373d713026c8c21f47
legion40216 <47054385+legion40216@users.noreply.github.com>
2022-07-09 06:31:39 +0500

    Update Todostores.js

--- src/Todostores.js
+++ src/Todostores.js
@@ -5,4 +5,9 @@ export const todoData = writable(JSON.parse(localStorage.getItem("todo_Data") ||
 todoData.subscribe(value => 
     localStorage.todo_Data = JSON.stringify(value));
 
- 
+    export const theme = writable(JSON.parse(localStorage.getItem("theme_Data") || "[]"))
+
+    theme.subscribe(value => 
+        localStorage.theme_Data = JSON.stringify(value));
+    
+     
